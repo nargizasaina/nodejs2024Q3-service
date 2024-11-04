ebc1a10dceeaea5a31e1ab54c23f534157a0f9ab
Nargiza Sainazarova <nargizasaina@gmail.com>
2024-11-04 15:02:43 +0600

    feat: add user created at and updated at fields

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,22 +19,27 @@ export class UserService {
   create(user: CreateUserDto) {
     const newUser = {
       id: uuidv4(),
+      login: user.login,
+      password: user.password,
       version: 1,
-      ...user
+      createdAt: Date.now(),
+      updatedAt: Date.now()
     };
     this.users.push(newUser);
     return newUser;
   }
 
   update(id: string, updatedUser: UpdatePasswordDto) {
+    const user = this.findOne(id);
+    
     this.users = this.users.map(user => {
       if (user.id === id && user.password === updatedUser.oldPassword) {
         const newVersion = user.version + 1;
         return { 
-          ...user, 
-          ...updatedUser, 
+          ...user,
           version: newVersion, 
-          password: updatedUser.newPassword };
+          password: updatedUser.newPassword,
+          updatedAt: Date.now() };
       }
       return user;
     });
